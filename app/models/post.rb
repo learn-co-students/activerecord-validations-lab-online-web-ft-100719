18a6b5dd83593e55f6f23a95ed3a900395d58e04
figueroar <raul.a.figueroa@hotmail.com>
2019-12-16 10:23:51 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with PostValidator
-  
 
+
+  
+  #validates :id, presence: true, uniqueness: true
+  #validates :title, presence: true
   #validates :content, presence: true, length: {in: 10..199}
-  #validates :summary, presence: true, length: {in: 10..199}
-  #validates :category, presence: true
+  #validates :summary, presence: true, length: {maximum: 250}
+  #validates :category, presence: true, inclusion: { in: %w(Fiction Non-Fiction)}
 end
--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,20 +1,27 @@
 class PostValidator < ActiveModel::Validator
   def validate(post)
     #binding.pry
-    if post.id = nil
-      post.save
+    #if post.id == nil
+      #post.errors[:id] << "Post is invalid without an iddddd"
+    #end
+    clickbait = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+    if clickbait.none?{ |p| p.match post.title}
+      post.errors[:title] << "Post is invalid without a titleeeeee"
     end
 
-    unless post.title = nil
-      post.errors[:title] << "Post is invalid without a title"
+    if post.content.length < 250
+      post.errors[:content] << "Post is invalid with too short contentttttt"
     end
-
-    unless post.content = nil
-      post.errors[:content] << "Post is invalid with too short content"
-    end
-
-
     
+    if post.summary.length > 250
+      post.errors[:summary] << "Post Summary Maximum is 250"
+    end
+
+    if post.category != ("Fiction" || "Non-Fiction")
+      post.errors[:category] << "Post Category options are: Fiction or Non-Fiction"
+    end
     
+  
+
   end 
 end
