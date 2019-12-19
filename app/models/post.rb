18d5e82f865e280977a3bc32797c1f1cf7d420fc
Alyssa Yates <alyssayates@Alyssas-MacBook-Air.local>
2019-12-19 17:38:15 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,13 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: %w("Fiction" "Non-Fiction") }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+    def clickbait
+        if self.title
+            unless self.title.match?(/Won't Believe/) || self.title.match?(/Secret/) || self.title.match?(/Top [number]/) || self.title.match?(/Guess/)
+                self.errors[:name] << "New Message, Make It Catchy!"
+            end
+        end 
+    end 
 end 
--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator 
     def validate(record)
         unless record.title.match?(/Won't Believe/) || record.title.match?(/Secret/) || record.title.match?(/Top [number]/) || record.title.match?(/Guess/)
-            record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+            record.errors[:name] << "New Message, Make It Catchy!"
         end 
     end 
 end 
