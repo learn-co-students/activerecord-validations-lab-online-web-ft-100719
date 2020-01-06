c2e997352cf70310b3be2a18989680bd7fed5e35
David Ingram <david@ingramaviation.com>
2020-01-06 12:49:58 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        clickbait_array = ["Won't Believe", "Secret", "Guess", "Top #{/\d/}"]
+        if title.present? && clickbait_array.none?{|item| title.match?(item)}
+            errors.add(:title, "More clickbait, please")
+        end
+    end
 end
