478765b3fd2e146ce1cbc37c76e51b1e63d9a149
Daniel Rooker <dannyrooks@gmail.com>
2020-01-07 13:41:22 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
-end
+    validates :title, presence: true, format:{with: /Won't Believe|Secret|Top 5|Guess/}
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: "Fiction Non-Fiction"}
+    validates :is_clickbait
+
+    
+    end
+    
+    
