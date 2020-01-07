491e5342d477a35533c41a43d2ef7ceb81616e05
lmjcbs <hello@liamjacobs.dev>
2020-01-07 10:36:35 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_clickbaity
+
+
+  def key_words
+    ["Won't Believe", "Secret", "Top", "Guess"]
+  end
+
+  def title_is_clickbaity
+    if title.present? && !key_words.any? { |word| title.include?(word) }
+      errors.add(:title, "Must include a clickbait-y word.")
+    end
+  end
+
 end
